0c406d6dbaac15ef85af569c841c721111e492f5
dinamashmush <mashmushdina@gmail.com>
2025-05-15 17:56:22 +0300

    .

--- FT.py
+++ FT.py
@@ -32,20 +32,20 @@ def plot_spectrum_from_wav_file(filename, start_time, end_time, bubble_name, plt
     active_data = data[int(start_time * samplerate):int(end_time * samplerate)]
     
     xf, yf = compute_fft(active_data, samplerate)
-    plot_spectrum(xf, yf, f"Spectrum from {start_time} to {end_time} seconds for {bubble_name}", plt)
+    plot_spectrum(xf, yf, f"Spectrum from {start_time} to {end_time} seconds for {bubble_name} of the sound waves", plt)
 
 
 def plot_spectrum(xf, yf, title, plt):
-    plt.figure(figsize=(12, 6))
+    # plt.figure(figsize=(12, 6))
     plt.plot(xf, yf)
-    plt.title(title)
-    plt.xscale('log')
+    plt.set_title(title)
+    plt.set_xscale('log')
     # plt.ylim(0, 6e8)
-    plt.xlabel("Frequency (Hz)")
-    plt.ylabel("Amplitude")
+    plt.set_xlabel("Frequency (Hz)")
+    plt.set_ylabel("Amplitude")
     ticks = [50, 100, 200, 300, 500, 1000, 2000, 5000, 10000, 20000]
-    plt.xticks(ticks, [str(int(t)) for t in ticks])
-    plt.xlim(1, 1000)
+    plt.set_xticks(ticks, [str(int(t)) for t in ticks])
+    plt.set_xlim(1, 1000)
     # plt.show()
 
 
